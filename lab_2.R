a487384cfa847b6c65ae04c8a6b528f101533647
Jake-Kistler <jakistler01@gmail.com>
2025-09-04 12:33:36 -0500

    We are calculating z values now. Now to find the two types of outliners from this

--- lab_2.R
+++ lab_2.R
@@ -6,5 +6,13 @@ dat <- read.csv("EPAGAS-1.XLS - Sheet1.csv")
 
 head(dat)
 names(dat)
-VEC_mpg = dat$MPG
-VEC_mpg
+mpg = dat$MPG
+mpg
+
+# Math time we've been asked to take mpg and turn it into z ok so I'm thinking z <- mpg()
+# what these args are I have no idea time to go to the sample code and it's there...
+# We also did this in class I have a mudworks file at C:\R_packages\Labs\In_class_mudworks\lab_2_mudworks
+z <- ((mpg - mean(mpg)/ sd(mpg)))
+z
+
+# These z values are the number of standard divs we are alway from the true standard div. And std div is how far we are away from the mean 
