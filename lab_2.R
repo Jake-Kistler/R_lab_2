34fa96d4fdf7fd843deb8b7da6a11f007e084f34
Jake-Kistler <jakistler01@gmail.com>
2025-09-04 12:07:45 -0500

    we can make and populate objects now! Oh boy watch out

---
+++ lab_2.R
@@ -0,0 +1,10 @@
+# task 1 and 2 are rather simple and have been only included in the rmd file 
+
+# read data in 
+dat <- read.csv("EPAGAS-1.XLS - Sheet1.csv")
+
+
+head(dat)
+names(dat)
+VEC_mpg = dat$MPG
+VEC_mpg
